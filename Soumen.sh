879cacd01f3d9721b4f89da8445c0b175ce451ab
soumenh <61101884+soumenh@users.noreply.github.com>
2020-05-06 10:28:14 +0530

    Update Soumen.sh

--- Soumen.sh
+++ Soumen.sh
@@ -52,7 +52,7 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" && sleep 0.3
             
    echo -e "\e[36m[NoRoot_Found] \e[34m Started no root progress.. .." && sleep 2
 if [ -d /data/data/com.tencent.iglite.ms ]; then
-if [ -d /data/user/0/com.tencent.iglite.ms/system/user/data/com.tencent.iglite ]; then
+if [ -d /data/user/0/com.tencent.iglite.ms/system/data/com.tencent.iglite ]; then
 
 echo -e "\e[36m [Found Pubg mobile lite]" && sleep 1 && clear
 echo -e "\e[36m [Injecting Fix crash]" && sleep 1 && clear
