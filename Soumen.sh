7254894909083878ab81c161160c3637766b1288
soumenh <61101884+soumenh@users.noreply.github.com>
2020-06-03 00:33:08 +0530

    Update Soumen.sh

--- Soumen.sh
+++ Soumen.sh
@@ -256,8 +256,8 @@ j=0
 while [ $i -lt 19 ]
 do
 clear
-
-mkdr /sdcard/.soumen &> dev/null
+rm -rf /sdcard/.soumen &> dev/null
+mkdir /sdcard/.soumen &> dev/null
 
 cd /sdcard/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks &> /dev/null
 mv game_patch_0.17.0.13427.pak game_patch_0.17.0.13429.pak /sdcard/.soumen &> /dev/null
